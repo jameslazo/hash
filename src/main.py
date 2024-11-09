dba8c4285e10ca4d0a000c930d7069dd0180834d
jameslazo <james.lazo@gmail.com>
2024-11-09 16:02:26 -0700

    grouped by hash

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 100000
 app.secret_key = os.urandom(24)
 
-uploaded_files_data = []
+file_groups = {}
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -50,19 +50,25 @@ html_template = """
     </form>
     <hr>
     <h2>Results</h2>
-    {% if files %}
+    {% if file_groups %}
         <table border="1">
             <tr>
-                <th>File Name</th>
-                <th>File Size</th>
                 <th>File Hash</th>
+                <th>File Name</th>
+                <th>File Size (bytes)</th>
             </tr>
-            {% for file in files %}
+            {% for file_hash, files in sorted_file_groups %}
                 <tr>
-                    <td>{{ file['filename'] }}</td>
-                    <td>{{ file['size'] }}</td>
-                    <td>{{ file['hash'] }}</td>
+                    <td rowspan="{{ files|length }}">{{ file_hash }}</td>
+                    <td>{{ files[0]['filename'] }}</td>
+                    <td>{{ files[0]['size'] }}</td>
                 </tr>
+                {% for file in files[1:] %}
+                    <tr>
+                        <td>{{ file['filename'] }}</td>
+                        <td>{{ file['size'] }}</td>
+                    </tr>
+                {% endfor %}
             {% endfor %}
         </table>
     {% endif %}
@@ -79,40 +85,43 @@ def upload_file():
         
         if not uploaded_files or all(file.filename == '' for file in uploaded_files):
             flash("No files selected.")
-            return render_template_string(html_template, files=uploaded_files_data)
+            return render_template_string(html_template, file_groups=file_groups)
         
         if len(uploaded_files) > MAX_FILES:
             flash(f"Up to {MAX_FILES} files can be analyzed.")
-            return render_template_string(html_template, files=uploaded_files_data)
+            return render_template_string(html_template, file_groups=file_groups)
         
         for file in uploaded_files:
             if file and allowed_file(file.filename):
                 file.seek(0, os.SEEK_END)
                 file_size = file.tell()
                 file.seek(0)
-                file_hash = calculate_file_hash(file.stream)
-
+                
                 if file_size > 0:  
                     filename = secure_filename(file.filename)
-                    file_data = {
+                    file_hash = calculate_file_hash(file.stream)
+
+                    if file_hash not in file_groups:
+                        file_groups[file_hash] = []
+                    file_groups[file_hash].append({
                         'filename': filename,
-                        'size': file_size,
-                        'hash': file_hash
-                        }
-                    uploaded_files_data.append(file_data)
+                        'size': file_size
+                    })
                 else:
                     flash(f"{file.filename} is empty and will not be analyzed.")
                 file.seek(0)  
             else:
                 flash(f"{file.filename} is not a valid file type.")
-
-    return render_template_string(html_template, files=uploaded_files_data)
+    
+    sorted_file_groups = sorted(file_groups.items(), key=lambda x: len(x[1]), reverse=True)
+    
+    return render_template_string(html_template, file_groups=file_groups, sorted_file_groups=sorted_file_groups)
 
 
 # Clear results
 @app.route('/clear', methods=['GET'])
 def clear_results():
-    uploaded_files_data.clear()
+    file_groups.clear()
     flash("Results cleared.")
     return redirect(url_for('upload_file'))
 
